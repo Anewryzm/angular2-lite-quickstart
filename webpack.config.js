9a27647d1e315cf6dab827f9f2a0d207908ed871
anewryzm <anewryzm@gmail.com>
2016-12-25 17:39:33 -0500

    webpack.config modified to build css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.ts',
+  entry: {
+    'app':'./src/main.ts',
+  },
   output: {
     path: './dist',
-    filename: 'app.bundle.js'
+    filename: '[name].bundle.js'
   },
   module:{
     loaders: [
       {test: /\.component.ts$/, loader: 'ts!angular2-template'},
       {test: /\.ts$/, exclude: /\.component.ts$/ , loader: 'ts'},
       {test: /\.html$/, loader: 'raw'},
-      {test: /\.css$/, loader: 'raw'},
+      {test: /\.css$/, exclude: /\.component.css$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap') },
+      {test: /\.css$/, include: /\.component.css$/, loader: 'raw'}
     ]
   },
   resolve:{
-    extensions: ['', '.js', '.ts', '.html', '.css']
+    extensions: ['', '.js', '.ts','.css']
   },
   plugins:[
     new HtmlWebpackPlugin({
@@ -27,6 +31,10 @@ module.exports = {
       proxy: 'localhost:8080'
     },{
       reload: false
-    })
-  ]
+    }),
+    new ExtractTextPlugin('[name].css')
+  ],
+  devServer: {
+    historyApiFallback: true,
+  }
 };
